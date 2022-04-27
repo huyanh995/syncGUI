349b7b5bdaa44eab795ae017b1874f639a9c0941
Huy Anh <huyanh995@gmail.com>
2022-04-26 22:35:46 -0400

    Enable all data fields

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import time
 import subprocess
+from collections import deque
 import logging
 import tkinter as tk
 from tkinter import filedialog, messagebox
@@ -15,23 +16,82 @@ import platform
 import socket
 import webbrowser
 import pandas as pd
+import yaml
 
 from utils import read_config, write_config, parse
 from control_OBS import OBS
+
+class AnnotationWindow(tk.Toplevel):
+    def __init__(self, master, path):
+        super().__init__(master = master)
+        self.path = path # Store current pilot ID path
+        self.title("Experiment Launcher")
+        self.geometry("450x200") 
+        self.length_label = tk.Label(text="Choose min clips length (ms)")
+        self.length_label.place(x = 20, y = 20)
+
+        self.scale = tk.Scale(from_ = 50, to = 10000, resolution = 50, orient = tk.HORIZONTAL, length = 400, command=self.calculate_number_clips)
+        self.scale.set(2000)
+
+        self.scale.place(x = 25, y = 45)
+
+        self.result_label = tk.Label(text = "Number of clips: ")
+        self.result_label.place(x = 20, y = 100)
+
+        self.result = tk.Label(text = "-1")
+        self.result.place(x = 140, y = 100)
+
+        self.create_playlist_button = tk.Button(text = "CREATE PLAYLIST", command = self.create_playlist_button)
+        self.create_mindwander_button = tk.Button(text = "MIND WANDERING", command = self.create_mindwander_button)
+
+        machine = platform.system()
+        if machine == "Windows":
+            self.create_playlist_button.place(x = 85, y = 140)
+            self.create_playlist_button["width"] = 15
+            self.create_playlist_button["height"] = 2
+            self.create_playlist_button["relief"] = tk.GROOVE
+            
+            self.create_mindwander_button.place(x = 245, y = 140)
+            self.create_mindwander_button["width"] = 15
+            self.create_mindwander_button["height"] = 2
+            self.create_mindwander_button["relief"] = tk.GROOVE
+
+        else:
+            self.create_playlist_button.place(x = 60, y = 135)
+            self.create_mindwander_button.place(x = 220, y = 135) 
+
+
+    # For Yifeng
+    def create_mindwand_playlist(self):
+        # To get current value of scaler
+        clip_len = self.scale.get()
+        pass
+
+    def create_playlist(self):
+        clip_len = self.scale.get()
+        pass
+
+    def calculate_number_clips(self, scale_var):
+
+        self.result.configure(text = int(scale_var) / 50)
+
 
 class App(tk.Tk):
     def __init__(self, machine):
         super().__init__()
         self.config = read_config("config.yml")
         self.path = None
+        self.calibration_results = deque(maxlen=500)
 
         # Logging system
-        self.logger = None # For logging, init when start recording.
+        self.logger = None # For logging, init when  recording.
 
         # OBS Init
         self.obs = OBS(self.config["OBS"])
         self.first_tick = None
+        self.start_time = None
         self.last_tick = None
+        self.stop_time = None
         self.obs_is_connect = False
 
         # GP3 threading variable
@@ -41,7 +101,8 @@ class App(tk.Tk):
         
         # Geometry
         self.title("Experiment Launcher")
-        self.geometry("450x210") 
+        self.geometry("450x260") 
+        # self.geometry("450x400") 
         self.resizable(False, False) 
         self.eval('tk::PlaceWindow . center')
         self.machine = machine
@@ -59,23 +120,37 @@ class App(tk.Tk):
         self.userID_label.place(x = 20,y = 20)
         self.userID_entry.place(x = 100,y = 20)
 
+        self.is_calibration = tk.BooleanVar()
+        self.is_calibration.set(self.config["calibration"])
+        self.calibration_checkbutton = tk.Checkbutton(self, text = "Force calibration", variable = self.is_calibration, onvalue = True, offvalue = False)
+        self.calibration_checkbutton.place(x = 20, y = 65)
+        self.calibrate_button = tk.Button(text="Calibrate", width = 6, command=self.start_calibration)
+        self.calibrate_button.place(x = 320, y = 65)
+
+        # self.is_precise_matching = tk.BooleanVar()
+        # self.is_precise_matching.set(self.config["preciseMatching"])
+        # self.precise_matching_button = tk.Checkbutton(self, text = "Precise matching (for SBU team)", variable = self.is_precise_matching,onvalue = True, offvalue = False)
+        # self.precise_matching_button.place(x = 20, y = 90)
+
+        
+
         self.output_label = tk.Label(self, text="Default output directory")
         self.output_dir_label = tk.Label(self, text=self.output_dir)
-        self.output_button = tk.Button(text="Change", command=self.choose_output)
-        self.output_label.place(x = 20, y = 70)
-        self.output_dir_label.place(x = 40, y = 100)
-        self.output_button.place(x = 330, y = 100)
+        self.output_button = tk.Button(text="Change", width = 6, command=self.choose_output)
+        self.output_label.place(x = 20, y = 120)
+        self.output_dir_label.place(x = 40, y = 150)
+        self.output_button.place(x = 320, y = 150)
 
         self.start_button = tk.Button(text="START RECORDING", command=self.click_start_recording)
         self.stop_button = tk.Button(text="STOP RECORDING", command=self.click_stop_recording, state=tk.DISABLED)
         if machine == "Windows":
-            self.start_button.place(x = 85, y = 150)
+            self.start_button.place(x = 85, y = 200)
             self.start_button["bg"] = "green"
             self.start_button["width"] = 15
             self.start_button["height"] = 2
             self.start_button["relief"] = tk.GROOVE
             
-            self.stop_button.place(x = 245, y = 150)
+            self.stop_button.place(x = 245, y = 200)
             self.stop_button["bg"] = "gray"
             self.stop_button["width"] = 15
             self.stop_button["height"] = 2
@@ -83,8 +158,8 @@ class App(tk.Tk):
 
             self.output_button["relief"] = tk.GROOVE
         else:
-            self.start_button.place(x = 60, y = 150)
-            self.stop_button.place(x = 220, y = 150)            
+            self.start_button.place(x = 60, y = 200)
+            self.stop_button.place(x = 220, y = 200)  
 
     def create_logger(self):
         # Config logger
@@ -116,6 +191,47 @@ class App(tk.Tk):
         self.userID_entry.configure(state=state)
         self.output_button.configure(state=state)
 
+    def start_calibration(self, recalibrate=False):
+        # Start calibration
+        # If calibration fails -> show a messagebox to do it again
+        # If yes, do it again
+        
+        # Clear calibration result
+        self.gp3.send(str.encode('<GET ID="CALIBRATE_CLEAR" />\r\n'))
+        # Show calibration screen
+        self.gp3.send(str.encode('<GET ID="CALIBRATE_SHOW" />\r\n'))
+        # During calibration, server will send <CAL tag 
+        # Catch it until there is no <CAL tag in the stream
+        self.gp3.send(str.encode('GET ID="CALIBRATE_RESULT_SUMMARY" />\r\n'))
+        # Hide the calibration window
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
+        # Get the summary result <ACK tag
+        self.gp3.send(str.encode('GET ID="CALIBRATE_RESULT_SUMMARY" />\r\n'))
+
+        result = None
+        avg_error = None
+        # Add to log
+        self.logger.info("CALIBRATION: Result {}. Average Error {}.".format(result, avg_error))
+        # Message box shows the result
+        # If pass 4/5 points -> Info box with Okay button, function returns True
+        # Else: Warning box with Yes/No question to retry. If yes -> Call start_calibration again, else returns False
+        if result < 4:
+            # Show Retry Cancel box
+            message = "{}/5 points passed. Average error {}.\nDo you want to redo the calibration?".format(result, avg_error)
+            answer = messagebox.askretrycancel(title="Calibration", message = message)
+            if answer:
+                self.start_calibration()
+            else:
+                return False
+        else:
+            # Show Info box
+            message = "{}/5 points passed. Average error {}.".format(result, avg_error)
+            messagebox.showinfo(title="Calibration", message = message)
+            return True
+        
+    def check_fps(self):
+        pass
+
     def gp3_connect(self):
         try:
             self.gp3.connect((self.config["GP3"]["host"], self.config["GP3"]["port"]))
@@ -130,10 +246,19 @@ class App(tk.Tk):
     
     def gp3_start_streaming(self):
         self.gp3.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_COUNTER" STATE="1" />\r\n'))
         self.gp3.send(str.encode('<SET ID="ENABLE_SEND_TIME" STATE="1" />\r\n'))
-        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_POG_FIX" STATE="1" />\r\n'))
-        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_CURSOR" STATE="1" />\r\n'))
         self.gp3.send(str.encode('<SET ID="ENABLE_SEND_TIME_TICK" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_POG_FIX" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_POG_LEFT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_POG_RIGHT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_POG_BEST" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_PUPIL_LEFT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_PUPIL_RIGHT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_EYE_LEFT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_EYE_RIGHT" STATE="1" />\r\n'))
+        self.gp3.send(str.encode('<SET ID="ENABLE_SEND_CURSOR" STATE="1" />\r\n'))
+
     
     def gp3_stop_streaming(self):
         self.gp3.send(str.encode('<SET ID="ENABLE_SEND_DATA" STATE="0" />\r\n'))
@@ -156,6 +281,7 @@ class App(tk.Tk):
 
     def gp3_process_data(self):
         # Pull data from global Queue 
+        # HEADER = ['TIME', 'TIME_TICK', 'FPOGX', 'FPOGY', 'FPOGV', 'CX', 'CY']
         HEADER = ['TIME', 'TIME_TICK', 'FPOGX', 'FPOGY', 'FPOGV', 'CX', 'CY']
         global gp3_buffer
         with open(os.path.join(self.path, "raw_gazepoints.csv"), "w", encoding="UTF8", newline="") as f:
@@ -167,6 +293,7 @@ class App(tk.Tk):
                     batch = batch.decode().split('\r\n')
                     for row in batch:
                         if len(row) > 0 and "REC" in row:
+                            # TODO: [] modify parse function
                             writer.writerow(parse(row))
             
             except Empty as e:
@@ -199,7 +326,7 @@ class App(tk.Tk):
         # Move raw_gazepoints.csv and obs.mkv to raw folder
         shutil.move(os.path.join(path, "raw_gazepoints.csv"), os.path.join(new_path, "raw_gazepoints.csv"))
         shutil.move(os.path.join(path, "obs.mkv"), os.path.join(new_path, "obs.mkv"))
-        shutil.move(os.path.join(path, "tick.txt"), os.path.join(new_path, "tick.txt"))
+        shutil.move(os.path.join(path, "tick.yml"), os.path.join(new_path, "tick.yml"))
 
 
     def check_condition(self):
@@ -211,7 +338,14 @@ class App(tk.Tk):
                 mes.append("Open OBS.")
             else:
                 self.obs_is_connect = True
-            
+        
+        # Check Precise Matching
+        if self.is_precise_matching.get():
+            status, fps = self.obs.getCurrentFps()
+            if fps != 61:
+                mes.append("Set OBS output video to 61fps to enable precise matching!")
+                self.is_precise_matching.set(False)
+
         # Check GP3 connection
         if not self.gp3_is_connect:
             out = self.gp3_connect()
@@ -232,6 +366,10 @@ class App(tk.Tk):
         return len(mes) == 0, mes
 
     def click_start_recording(self):
+        # Write is_calibration, precise_matching to config file for later uses
+        self.config["calibration"] = self.is_calibration.get()
+        # self.config["preciseMatching"] = self.is_precise_matching.get()
+        write_config(self.config)
 
         # Check condition and start OBS, GP3 connections
         out, mes = self.check_condition()
@@ -251,7 +389,7 @@ class App(tk.Tk):
         # Config OBS output to pilot ID path
         out = self.obs.setOutFolder(self.path)
         if not out:
-            messagebox.showwarning(title="Warning", message="Can not start recording. Contact SBU team!")
+            messagebox.showwarning(title="Warning", message="Can not create pilot folder. Contact SBU team!")
             self.control_field(True)
             return
 
@@ -262,35 +400,41 @@ class App(tk.Tk):
         t1.start()
         t2.start()
         
-        time.sleep(2.0) # Waiting a bit for GP3 to start. Gp3 must start before OBS recording.
+        # Set recording button to disabled 
+        self.start_button.configure(state=tk.DISABLED, text="RECORDING", bg="gray")
+        self.stop_button.configure(state=tk.NORMAL, bg="red")
+        self.userID_entry.configure(state=tk.DISABLED)
+
+        #time.sleep(2.0) # Waiting a bit for GP3 to start. Gp3 must start before OBS recording.
+        self.after(5000) # 10s
         # Start OBS Recording
-        out = self.obs.startRecording()
-        self.first_tick = time.monotonic_ns()
+        out, first_tick, start_time = self.obs.startRecording()
+        #self.first_tick = time.monotonic_ns()
 
         if not out:
             messagebox.showwarning(title="Warning", message="Can not start recording. Contact SBU team!")
             self.control_field(True)
             return
         
+        self.first_tick = first_tick
+        self.start_time = start_time
+        
         # Open Learning Material Website
         #webbrowser.open_new_tab(self.config["LearningModule"])
-
-        # Set recording button to disabled 
-        self.start_button.configure(state=tk.DISABLED, text="RECORDING", bg="gray")
-        self.stop_button.configure(state=tk.NORMAL, bg="red")
-        self.userID_entry.configure(state=tk.DISABLED)
 
 
     def click_stop_recording(self):
         
         # Stop OBS Recording
-        out = self.obs.stopRecording()
-        self.last_tick = time.monotonic_ns()
+        out, last_tick, stop_time = self.obs.stopRecording()
+        self.last_tick = last_tick
+        self.stop_time = stop_time
         if not out:
             messagebox.showwarning(title="Warning", message="Can not stop recording. Do it manually and contact SBU team!")
             self.control_field(True)
 
-        time.sleep(2.0) # Wait awhile before stop GP3.
+        #time.sleep(2.0) # Wait awhile before stop GP3.
+        self.after(2000)
 
         # Disable GP3 send data
         self.gp3_stop_streaming()
@@ -301,12 +445,19 @@ class App(tk.Tk):
         self.userID_entry.configure(state=tk.NORMAL)
 
         # Save first/last tick to csv file
-        with open(os.path.join(self.path, "tick.txt"), "w") as f:
-            f.write("{}\n{}".format(self.first_tick, self.last_tick))
+        with open(os.path.join(self.path, "tick.yml"), "w") as f:
+            time_data = {"CPUTick": {"Start": self.first_tick, "Stop": self.last_tick}, 
+                        "SysTime": {"Start": self.start_time, "Stop": self.stop_time}}
+            #f.write("{}\n{}".format(self.first_tick, self.last_tick))
+            output = yaml.dump(time_data, f)
 
-        time.sleep(2.5) # Wait for CSV Writer finish the job
+        #time.sleep(5) # Wait for CSV Writer finish the job
+        self.after(5000)
 
         self.postprocess(self.path) # Postprocessing gazepoints.csv and obs.mkv
+
+        # Open Postprocessing tool
+        AnnotationWindow(self, self.path)
 
         self.logger = None # Avoid uncontrolled log
 
--- control_OBS.py
+++ control_OBS.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from obswebsocket import obsws, requests
+import time
 
 class OBS():
     def __init__(self, config):
         host, port, password = config["host"], config["port"], config["password"]
-        self.obs = obsws(host, port, password)
+        self.ws = obsws(host, port, password)
         self.logger = None
 
     def set_logger(self, logger):
@@ -11,30 +13,50 @@ class OBS():
 
     def connect(self):
         try:
-            self.obs.connect()
+            self.ws.connect()
             return True
         except Exception as e:
             self.logger.exception("Unexpected exception in OBS_connect.")
             return False
 
+    def getCurrentFps(self):
+        out = self.ws.call(requests.GetVideoInfo())
+        if not out.status:
+            self.logger.exception("Failed to get video info.")
+            return False, -1
+        return True, out.getFps()
 
     def setOutFolder(self, path):
-        out = self.obs.call(requests.SetRecordingFolder(path))
-        if not out.data:
+        out = self.ws.call(requests.SetRecordingFolder(path))
+        if not out.status:
             self.logger.exception("Failed to set output directory in OBS. Path {}, out {}".format(path, out))
             return False
         return True        
 
     def startRecording(self):
-        out = self.obs.call(requests.StartRecording())
-        if not out.data:
+        out = self.ws.call(requests.StartRecording())
+        start = time.monotonic_ns()
+        start_time = datetime.now()
+        if not out.status:
             self.logger.exception("Failed to start recording. Out {}".format(out))
-            return False
-        return True
+            return False, None, None
+        return True, start, start_time
 
     def stopRecording(self):
-        out = self.obs.call(requests.StopRecording())
-        if not out.data:
+        out = self.ws.call(requests.StopRecording())
+        end = time.monotonic_ns()
+        stop_time = datetime.now()
+        if not out.status:
             self.logger.exception("Failed to stop recording. Out {}".format(out))
-            return False
-        return True
+            return False, None, None
+        return True, end, stop_time
+
+if __name__ == "__main__":
+    # DEBUG
+    host = "localhost"
+    port = 4444
+    password = "sbu"
+
+    ws = obsws(host, port, password)
+    ws.connect()
+    print(ws.call(requests.GetVideoInfo()).getFps())
